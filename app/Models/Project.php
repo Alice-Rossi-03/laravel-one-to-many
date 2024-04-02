a5b3d81e92f35bbd08020332b6fe24c7d4f8e75d
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-04-02 12:15:14 +0200

    feat (all) /seeding-database

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
+use App\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'cover', 'slug'];
+    protected $fillable = ['title', 'description', 'cover', 'slug', 'type_id'];
 
     public static function generateSlug($title){
         return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo{
+        return $this->belongsTo(Type::class);
     }
 }
